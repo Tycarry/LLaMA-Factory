8ac60ee54db525a79cd4a51bda86cd79c3382739
ty <2501160661@qq.com>
2025-04-24 23:24:40 +0800

    添加微调脚本

---
+++ sft.sh
@@ -0,0 +1,30 @@
+#CUDA_VISIBLE_DEVICES=0 llamafactory-cli train \
+CUDA_VISIBLE_DEVICES=0 python -m ./src/train.py \
+    --stage sft \
+    --do_train \
+    --model_name_or_path /media/codingma/LLM/llama3/Meta-Llama-3-8B-Instruct \
+    --dataset alpaca_gpt4_zh,identity,adgen_local \
+    --dataset_dir ./data \
+    --template llama3 \
+    --finetuning_type lora \
+    --output_dir ./saves/LLaMA3-8B/lora/sft \
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --cutoff_len 1024 \
+    --preprocessing_num_workers 32 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --lr_scheduler_type cosine \
+    --logging_steps 50 \
+    --warmup_steps 20 \
+    --save_steps 100 \
+    --eval_steps 50 \
+    --evaluation_strategy steps \
+    --load_best_model_at_end \
+    --learning_rate 5e-5 \
+    --num_train_epochs 5.0 \
+    --max_samples 1000 \
+    --val_size 0.1 \
+    --plot_loss \
+    --fp16
